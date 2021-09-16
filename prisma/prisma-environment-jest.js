b35e7743a2cfe1343f8ec71db3bd756129709e4c
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2021-09-16 17:34:50 -0300

    fix: jest

--- prisma/prisma-environment-jest.js
+++ prisma/prisma-environment-jest.js
@@ -1,11 +1,13 @@
 const NodeEnvironment = require('jest-environment-node')
 const { execSync } = require('child_process')
 const { resolve } = require('path')
+const mysql = require('mysql2')
 
 require('dotenv').config({
   path: resolve(__dirname, '..', '.env.test')
 })
 
+const con = mysql.createConnection(process.env.DATABASE_URL)
 const prismaCli = "./node_modules/.bin/prisma"
 const tsNode = "./node_modules/.bin/ts-node"
 
@@ -15,16 +17,19 @@ class CustomEnvironment extends NodeEnvironment {
   }
   
   setup() {
-    this.global.process.env.DATABASE_URL = process.env.DATABASE_URL
+    process.env.DATABASE_URL = `${process.env.DATABASE_URL}/supertest`
+    this.global.process.env.DATABASE_URL = `${process.env.DATABASE_URL}/supertest`
     this.global.process.env.ADMIN_EMAIL = process.env.ADMIN_EMAIL
     this.global.process.env.ADMIN_PASS = process.env.ADMIN_PASS
+
+    con.query('CREATE DATABASE IF NOT EXISTS supertest')
 
     execSync(`${prismaCli} migrate dev`)
     execSync(`${tsNode} prisma/seed.ts`)
   }
 
   teardown() {
-    execSync(`${prismaCli} migrate reset`)
+    con.query('DROP DATABASE supertest')
   }
 }
 
