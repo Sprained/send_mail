06cf8093193f07b198b0675bde5bd1042f43f46b
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2021-09-23 10:47:14 -0300

    refact: change database

--- prisma/prisma-environment-jest.js
+++ prisma/prisma-environment-jest.js
@@ -1,36 +1,42 @@
 const NodeEnvironment = require('jest-environment-node')
 const { execSync } = require('child_process')
+const { v4: uuid } = require('uuid')
 const { resolve } = require('path')
-const mysql = require('mysql2')
+const { Client } = require('pg')
 
 require('dotenv').config({
   path: resolve(__dirname, '..', '.env.test')
 })
 
-const con = mysql.createConnection(process.env.DATABASE_URL)
 const prismaCli = "./node_modules/.bin/prisma"
 const tsNode = "./node_modules/.bin/ts-node"
 
 class CustomEnvironment extends NodeEnvironment {
   constructor(config) {
     super(config)
+    
+    this.uuid = `code_schema_${uuid()}`
+    this.url = `${process.env.DATABASE_URL}${this.uuid}`
   }
   
   setup() {
-    process.env.DATABASE_URL = `${process.env.DATABASE_URL}/supertest`
-    this.global.process.env.DATABASE_URL = `${process.env.DATABASE_URL}/supertest`
+    process.env.DATABASE_URL = this.url
+    this.global.process.env.DATABASE_URL = this.url
     this.global.process.env.ADMIN_EMAIL = process.env.ADMIN_EMAIL
     this.global.process.env.ADMIN_PASS = process.env.ADMIN_PASS
-
-    con.query('CREATE DATABASE IF NOT EXISTS supertest')
 
     execSync(`${prismaCli} migrate dev`)
     execSync(`${tsNode} prisma/seed.ts`)
   }
 
-  teardown() {
-    con.query('DROP DATABASE supertest')
-    con.destroy()
+  async teardown() {
+    const client = new Client({
+      connectionString: this.url
+    })
+
+    await client.connect()
+    await client.query(`DROP SCHEMA IF EXISTS "${this.uuid}" CASCADE`)
+    await client.end()
   }
 }
 
